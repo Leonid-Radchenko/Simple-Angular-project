43478e0059045b17806b04af31c0879d11c6a4bf
Leonid Radchenko <radchenkoxxleonid@gmail.com>
2024-11-18 22:36:33 +0200

    add navigation

--- task-3.2/src/app/app.routes.ts
+++ task-3.2/src/app/app.routes.ts
@@ -3,6 +3,10 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 
 export const routes: Routes = [
-  { path: '', component: MainPageComponent},
-  { path: 'contacts', component: ContactPageComponent},
+  { path: '', 
+    component: MainPageComponent
+  },
+  { path: 'contacts', 
+    component: ContactPageComponent
+  },
 ];
