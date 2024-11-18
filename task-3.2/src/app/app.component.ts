7498dc1328ebf62132758f9dfb4171e4665b2c59
Leonid Radchenko <radchenkoxxleonid@gmail.com>
2024-11-18 23:42:07 +0200

    relocated navbar to header component and add styles

--- task-3.2/src/app/app.component.ts
+++ task-3.2/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    RouterLink
-  ],
+    RouterOutlet,
+    HeaderComponent,
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
