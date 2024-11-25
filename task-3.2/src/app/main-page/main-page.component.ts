5185a7ba917951e767eb5edf80f09b892c2cc621
Leonid Radchenko <radchenkoxxleonid@gmail.com>
2024-11-25 21:13:01 +0200

    added products to main page

--- task-3.2/src/app/main-page/main-page.component.ts
+++ task-3.2/src/app/main-page/main-page.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent, CommonModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
-export class MainPageComponent {
+export class MainPageComponent implements OnInit {
+  products: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any[]>('assets/data/products.json').subscribe({
+      next: (data) => {
+        this.products = data;
+      },
+      error: (err) => {
+        console.error('Error loading products:', err);
+      }
+    });
+  }
 }
